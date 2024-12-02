340143b3da04ff83ccc91bd20ef362d8dc395968
Ishu Baghel <baghelishu09@gmail.com>
2024-12-02 23:38:09 +0530

    rename file

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 80;
+const path = require('path');
+
+app.use('/assets',express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
